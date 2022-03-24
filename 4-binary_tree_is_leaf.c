97fac29aa2ae4ae8f32aa12c9e8becf00e3f576e
lulimsky <3337@holbertonschool.com>
2022-03-24 16:36:42 -0700

    t4_betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,8 +6,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-  if (!node || node->left || node->right)
-    return (0);
-  else
-    return (1);  
+	if (!node || node->left || node->right)
+		return (0);
+		else
+			return (1);
 }
